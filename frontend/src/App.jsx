1dd216bee424cdf3918c640848aa01da8c9660a6
KiyoshiRei <piyyogamr@gmail.com>
2025-05-08 00:28:03 +0530

    added the pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import HomePage from "./pages/HomePage.jsx"
+import SignUpPage from "./pages/SignUpPage.jsx"
+import SignInPage from "./pages/SignInPage.jsx"
+import SettingsPage from "./pages/SettingsPage.jsx"
+import ProfilePage from "./pages/ProfilePage.jsx"
+
+import { Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar.jsx"
 
 const App = () => {
   return (
-    <div className='text-red-500'>hello world</div>
-  )
+    <div>
+       
+       <Navbar />
+
+       <Routes>
+          <Route path='/' element={<HomePage />}/>
+          <Route path='/signup' element={<SignUpPage />}/>
+          <Route path='/signin' element={<SignInPage />}/>
+          <Route path='/settings' element={<SettingsPage />}/>
+          <Route path='/profile' element={<ProfilePage />}/>
+       </Routes>
+    </div>
+  );
 }
 
 export default App
